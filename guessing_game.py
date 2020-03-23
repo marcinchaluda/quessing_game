ce6fbf62949cfa3395a480aef9ed1691283912c3
marcinchaluda <chaluda@interia.pl>
2020-03-23 18:58:55 +0100

    add method to create a list of random numbers

--- guessing_game.py
+++ guessing_game.py
@@ -1,49 +1,69 @@
-import math # import math module
-import random # import random module
-a = [] # creating an empty list
-a.append(random.randint(1, 99)) # add one element to the end of the empty list
-a.append(random.randint(1, 99)) # add one element to the end of the empty list
-a.append(random.randint(1, 99)) # add one element to the end of the empty list
-a.append(random.randint(1, 99)) # add one element to the end of the empty list
-a.append(random.randint(1, 99)) # add one element to the end of the empty list
-a.append(random.randint(1, 99)) # add one element to the end of the empty list
-a.append(random.randint(1, 99)) # add one element to the end of the empty list
-a.append(random.randint(1, 99)) # add one element to the end of the empty list
-a.append(random.randint(1, 99)) # add one element to the end of the empty list
-a.append(random.randint(1, 99)) # add one element to the end of the empty list
-for i in range(10): # iterating from 0 to 9 which is the length of the list
-    g = int(input("Enter an integer from 1 to 99: ")) # convert user input as integer and assign it to a variable 
-    while a[i] != g: # looping through consecutive elements in a list until user input is equal to given element
-        if g < a[i]: # check if user input is lower than given list element
-            print("guess is low") # print hint that given number is lower than user input
-            g = int(input("Enter an integer from 1 to 99: ")) # convert user input as integer and assign it to a variable
-        elif g > a[i]: # check if user input is higher than given list element
-            print("guess is high") # print hint that given number is higher than user input
-            g = int(input("Enter an integer from 1 to 99: ")) # convert user input as integer and assign it to a variable
-        else: # if above conditions are not met execute below code
-            break # exit from loop even if loop did not finish its working 
-    print("you guessed it!") # print information that your input is equal to given element from a list
+import math
+from random import randint
 
-b = []# creating an empty list
-b.append(random.randint(1, 49))# add one element to the end of the empty list
-b.append(random.randint(1, 49))# add one element to the end of the empty list
-b.append(random.randint(1, 49))# add one element to the end of the empty list
-b.append(random.randint(1, 49))# add one element to the end of the empty list
-b.append(random.randint(1, 49))# add one element to the end of the empty list
-b.append(random.randint(1, 49))# add one element to the end of the empty list
-b.append(random.randint(1, 49))# add one element to the end of the empty list
-b.append(random.randint(1, 49))# add one element to the end of the empty list
-b.append(random.randint(1, 49))# add one element to the end of the empty list
-b.append(random.randint(1, 49))# add one element to the end of the empty list
-for i in range(10): # iterating from 0 to 9 which is the length of the list
-    g = int(input("Enter an integer from 1 to 49: ")) # convert user input as integer and assign it to a variable
-    while b[i] != g: # looping through consecutive elements in a list until user input is equal to given element
-        if g < b[i]: # check if user input is lower than given list element
-            print("guess is low") # print hint that given number is lower than user input
-            g = int(input("Enter an integer from 1 to 49: ")) # convert user input as integer and assign it to a variable
-        elif g > b[i]: # check if user input is higher than given list element
-            print("guess is high") # print hint that given number is higher than user input
-            g = int(input("Enter an integer from 1 to 49: ")) # convert user input as integer and assign it to a variable
-        else: # if above conditions are not met execute below code
-            break # exit from loop even if loop did not finish its working
-    print("you guessed it!") # print information that your input is equal to given element from a list
+
+def create_random_number_list(list_lenght:int, scope_start:int, scope_end:int):
+    random_number_list = []
+    for element in range(list_lenght):
+        random_number_list.append(randint(scope_start, scope_end))
+    return random_number_list, scope_start, scope_end
+
+
+def handle_user_input(scope_start:int, scope_end:int):
+    return int(input(f"Enter an integer from {scope_start} to {scope_end}: "))
+
+
+def handle_number_input_comparison(user_number:int, number_to_compare:int):
+    if user_number < number_to_compare:
+        print("guess is low")
+    elif user_number > number_to_compare:
+        print("guess is high")
+    else:
+        return False
+
+def game_logic(list_lenght:int, scope_start:int, scope_end:int):
+    random_numbers, scope_start, scope_end = create_random_number_list(list_lenght, scope_start, scope_end)
+    for index in range(len(random_numbers)):
+        user_number = handle_user_input(scope_start, scope_end)
+        while random_numbers[index] != user_number:
+            number_to_compare = random_numbers[index]
+            comparison_result = handle_number_input_comparison(user_number, number_to_compare)
+            user_number = handle_user_input(scope_start, scope_end)
+            if comparison_result == False:
+                break
+        print("you guessed it!")
+
+
+
+def run_application():
+    game_logic(10, 1, 99)
+    game_logic(10, 1, 49)
+
+# for i in range(10):
+#     g = int(input("Enter an integer from 1 to 99: ")) 
+#     while a[i] != g:
+#         if g < a[i]:
+#             print("guess is low")
+#             g = int(input("Enter an integer from 1 to 99: "))
+#         elif g > a[i]:
+#             print("guess is high")
+#             g = int(input("Enter an integer from 1 to 99: "))
+#         else:
+#             break
+#     print("you guessed it!")
+
+
+# for i in range(10):
+#     g = int(input("Enter an integer from 1 to 49: "))
+#     while b[i] != g:
+#         if g < b[i]:
+#             print("guess is low")
+#             g = int(input("Enter an integer from 1 to 49: "))
+#         elif g > b[i]:
+#             print("guess is high")
+#             g = int(input("Enter an integer from 1 to 49: "))
+#         else:
+#             break
+#     print("you guessed it!")
+if __name__ == "__main__":
+    run_application()
